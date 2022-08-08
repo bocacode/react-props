b2d4c0076e256abcb515f059a29a346d91825760
Todd Albert <todd@bocacode.com>
2022-08-08 14:13:02 -0400

    passed props from App to Main

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,9 +1,12 @@
 import React from "react";
 
-export default function Main() {
+export default function Main(props) {
+  const { name, total, isAmazing, fruit } = props;
   return (
     <main>
-      <h1>Main</h1>
+      <h1>Hello {name}</h1>
+      <h2>Total: {total}</h2>
+      {isAmazing && <h2>Fruit: {fruit}</h2>}
     </main>
   )
 }
