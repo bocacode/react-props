621e0498cb32bd92b25011fa088e7c35755939a4
Todd Albert <todd@bocacode.com>
2022-08-08 14:42:41 -0400

    pass an object as props

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,9 +1,18 @@
 import React from "react";
+import StudentCard from "./StudentCard";
+
+const student = {
+  name: 'Khali Gopaul',
+  age: 37,
+  id: 'r64j83',
+  term: 'Su22',
+}
 
 function Main({ name }) {
   return (
     <main>
       <h1>Hello {name}</h1>
+      <StudentCard student={student} />
     </main>
   )
 }
