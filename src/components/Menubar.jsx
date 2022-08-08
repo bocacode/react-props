a0b23d0e040c457a917f64be8c4efdfc83104c8c
Todd Albert <todd@bocacode.com>
2022-08-08 15:00:08 -0400

    fake login / logout

--- src/components/Menubar.jsx
+++ src/components/Menubar.jsx
@@ -1,9 +1,13 @@
 import React from "react";
 
-function Menubar() {
+function Menubar({ name, setName }) {
   return (
-    <nav>
-      Menubar
+    <nav style={{ display: 'flex', justifyContent: 'space-between'}}>
+      <span>Menubar</span>
+      {name
+        ? <button onClick={() => setName(null)}>Logout</button>
+        : <button onClick={() => setName('Mason')}>Login</button>
+      }
     </nav>
   )
 }
