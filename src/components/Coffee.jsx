73a805b943142f8a792ac13a2c58bf57d1db8af6
Todd Albert <todd@bocacode.com>
2022-08-08 16:53:13 -0400

    rewrote fetch with async/await

--- src/components/Coffee.jsx
+++ src/components/Coffee.jsx
@@ -2,17 +2,22 @@ import React, { useState } from 'react';
 
 function Coffee() {
   const [coffeeList, setCoffeeList] = useState();
-  const getCoffee = (temperature) => {
-    // fetch coffee api
-    fetch(`https://api.sampleapis.com/coffee/${temperature}`)
-      .then(results => results.json())
-      .then(data => setCoffeeList(data))
-      .catch(err => alert(err))
+
+  const getCoffeeSync = async (temperature) => {
+    try {
+      const results = await fetch(`https://api.sampleapis.com/coffee/${temperature}`);
+      const data = await results.json();
+      setCoffeeList(data);
+    }
+    catch(err) {
+      alert(err);
+    }
   }
+
   return (
     <>
-      <button onClick={() => getCoffee('hot')}>HOT</button>
-      <button onClick={() => getCoffee('iced')}>ICED</button>
+      <button onClick={() => getCoffeeSync('hot')}>HOT</button>
+      <button onClick={() => getCoffeeSync('iced')}>ICED</button>
       <section>
         <h2>Coffee List</h2>
         {!coffeeList
